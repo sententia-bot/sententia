e058b04e3cea40740681aa6946ec6ae9fc298c4a
albin dalbert <albin.dalbert@protonmail.com>
2026-03-09 19:40:12 +0000

    stuff

--- update-deploy.sh
+++ update-deploy.sh
@@ -8,7 +8,7 @@ echo "Fetching freshest base image (tag: $TAG)..."
 sudo nerdctl --namespace k8s.io pull ghcr.io/openclaw/openclaw:$TAG
 
 echo "Building directly into Kubernetes' brain..."
-sudo nerdctl --namespace k8s.io build --build-arg OPENCLAW_TAG=$TAG  -t ghcr.io/albindalbert/sententia:latest .
+sudo nerdctl --namespace k8s.io build --no-cache --build-arg OPENCLAW_TAG=$TAG  -t ghcr.io/albindalbert/sententia:latest .
 
 echo "Nuking the old pod..."
 kubectl delete pod openclaw-gateway-0 -n openclaw
